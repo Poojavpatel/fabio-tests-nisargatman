8a7e81229a47cd21860e3fcfff837949cae58549
Pooja <pooja.patel@mosaicwellness.in>
2022-01-09 01:14:03 +0530

    BE added database queries

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
+from starlette.requests import Request
 from starlette.routing import Route
 from databases import Database
 
@@ -9,13 +10,23 @@ async def homepage(request):
   return JSONResponse({'hello': 'world'})
 
 async def getDocuments(request):
-  return JSONResponse({'cat': 'yes'})
+  query = "SELECT * FROM CatDocuments"
+  rows = await database.fetch_all(query=query)
+  data = [dict(row) for row in rows]
+  return JSONResponse(data)
+
+async def addDocuments(request: Request):
+  payload = await request.json()
+  query = "INSERT INTO CatDocuments(title, type, position) VALUES (:title, :type, :position)"
+  await database.execute(query=query, values=payload)
+  return JSONResponse({"message" : "Data inserted successfully"})
 
 async def updateDocuments(request):
-  return JSONResponse({'yup': 'yes'})
+  return JSONResponse({'message': 'Data updated successfully'})
 
 app = Starlette(debug=True, routes=[
   Route('/', homepage),
   Route('/getDocuments', getDocuments),
-  Route('/updateDocuments', updateDocuments, methods=['POST'])
+  Route('/updateDocuments', updateDocuments, methods=['POST']),
+  Route('/addDocuments', addDocuments, methods=['POST'])
 ])
